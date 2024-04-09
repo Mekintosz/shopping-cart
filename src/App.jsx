d9534b4d91b7210f650e9e4991467cacfc827b84
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2024-04-10 00:14:17 +0200

    create rotes, navbar, shop and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar } from "./components/navbar.jsx"
+import { Shop } from "./pages/shop/shop.jsx"
+import { Cart } from "./pages/cart/cart.jsx"
 
 function App() {
  
 
   return (
-    <>
-     
-    </>
+    <div className='App'>
+     <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path='cart'element={<Cart />} />
+      </Routes>
+     </Router>
+    </div>
   )
 }
 
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,12 @@
+export const Shop = () => {
+    return (
+    <div className="shop">
+        <div className="shop-title">
+            <h1>Mikes Shop</h1>
+        </div>
+        <div className="products">
+            
+        </div>
+    </div>
+    )
+}
