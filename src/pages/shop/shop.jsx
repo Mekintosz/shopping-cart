8c111daee653ba554e9147e4f69a7d8027d153d9
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2024-04-10 16:21:08 +0200

    create product component

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,11 +1,16 @@
+import { PRODUCTS } from "../../products"
+import { Product } from "../../components/product"
+
 export const Shop = () => {
     return (
     <div className="shop">
         <div className="shop-title">
-            <h1>Mikes Shop</h1>
+            <h1>Mikes Bike Shop</h1>
         </div>
         <div className="products">
-            
+            {PRODUCTS.map((product) => (
+                <Product data={product} />
+            ))}
         </div>
     </div>
     )
---
+++ src/products.js
@@ -0,0 +1,38 @@
+import bike1 from "./assets/bike1.webp";
+import bike2 from "./assets/bike2.webp";
+import bike3 from "./assets/bike3.webp";
+import bike4 from "./assets/bike4.webp";
+import bike5 from "./assets/bike5.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Trek black",
+    price: 1200,
+    productImage: bike1,
+  },
+  {
+    id: 2,
+    productName: "Trek 2",
+    price: 900,
+    productImage: bike2,
+  },
+  {
+    id: 3,
+    productName: "Trek 3",
+    price: 1050,
+    productImage: bike3,
+  },
+  {
+    id: 4,
+    productName: "Trek 4",
+    price: 830,
+    productImage: bike4,
+  },
+  {
+    id: 5,
+    productName: "Trek 5",
+    price: 1150,
+    productImage: bike5,
+  },
+];
