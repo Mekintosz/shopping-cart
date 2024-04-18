9e0ffcae65cd16f840e227d64d452b9a6d144245
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2024-04-18 19:55:15 +0200

    create context

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,4 +1,13 @@
 export const Product = (props) => {
     const {id, productName, price, productImage} = props.data
-    return <div>product</div>
+    return <div className="product">
+        <img src={productImage}/>
+        <div className="description">
+            <p>
+                <b>{productName}</b>
+            </p>
+            <p>{price} PLN</p>
+            <button className="addToCartBtn">Add to cart</button>
+        </div>
+    </div>
 }
